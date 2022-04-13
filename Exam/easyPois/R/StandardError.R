27d382a2ae841cbab68d80dfeb8e88d0b865b057
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-12 21:21:26 -0500

    even more comments

--- Exam/easyPois/R/StandardError.R
+++ Exam/easyPois/R/StandardError.R
@@ -1,5 +1,10 @@
-#So first I'm going to do this:
+#Now, I'll just create this function as if we can only do SEtype=basic
+standardError<-function(y, SEtype, B=20){
+    n<-length(y)
+    #Getting n again
 
-standardError<-function(y, SEtype, B=20)
+    the_se<-sqrt(mle(y)/n)
+    #this is just the se assuming that I'm setting the method to pass the mle through here.
+    #I'm not too sure about this yet.
+}
 
-  #So, I want it to take in y, SE type, and B, which I'll set at 20 because that's my favorite number.
