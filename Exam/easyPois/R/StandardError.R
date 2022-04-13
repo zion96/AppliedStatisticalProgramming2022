77a78c5b5581f3ff0565e6f5a71dcc5892619a63
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-12 22:17:44 -0500

    added if and else if
    now, I set if basic then... and else if bootstrap then ...

--- Exam/easyPois/R/StandardError.R
+++ Exam/easyPois/R/StandardError.R
@@ -1,13 +1,20 @@
 
 standardError<-function(y, SEtype, B=20){
+  #Run the if statement when SEtype is basic
+
+  if(SEtype=="basic"){
+
+
     n<-length(y)
     #Getting n again
 
     the_se<-sqrt(mle(y)/n)
-    #this is just the se assuming that I'm setting the method to pass the mle through here.
-    #I'm not too sure about this yet.
+  }
 
-    the_samples<-matrix(unlist(lapply(c(1:B), FUN=sample(y, n , replace=TRUE))), nrow=n, ncol = B)
+  #Run the else if statement when SEtype is bootstrap
+
+    else if (SEtype=="bootstrap"){
+      the_samples<-matrix(unlist(lapply(c(1:B), FUN=sample(y, n , replace=TRUE))), nrow=n, ncol = B)
 
     #Just using lappy to to get the samples as defined in the exam
 
@@ -18,9 +25,10 @@ standardError<-function(y, SEtype, B=20){
     #Get mle std
 
     the_se<-mle_SD
+  }
 
 
-
+return(the_se)
 
 
 }
