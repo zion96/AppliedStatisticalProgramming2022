32effe2e84bb209e25d99b8a2dd70286031c39e3
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-13 00:00:27 -0500

    documentation for logLik

--- Exam/easyPois/R/logLik.R
+++ Exam/easyPois/R/logLik.R
@@ -1,3 +1,25 @@
+#'Log Likelihood for Poisson Model
+#'
+#'This calculates the log likelihood in a \code{PoisMLE} object. This is for internal use.
+#'The following function calculates log likelihood for a vector of given data where rate parameter
+#'\eqn{\lambda} is assumed. This function performs the log likelihood equation given by
+#'\deqn{LL(\lambda)=-n\lambda-\sum^n_[i=1]\ln(y_i!)+\ln(y_i!)+\ln(\lambda)\sum^n_[i=1]y_i}.
+#'The value that the below function gives is returned when an object in the class \code{PoisMLE} is initialized.
+#'
+#'@param y is a vector for which all entries are strictly non-negative reals
+#'@param lambda is the assumed rate for a Poisson process or Poisson distribution
+#'
+#'@return The log likelihood value for objects in the class \code{PoisMLE}
+#'
+#'@author Zion Little
+#'@seealso PoisMLE-class, MLE,StandardError
+#'@rdname logLik
+#'@aliases logLik
+#'
+#'@keywords internal
+#'
+#'@export
+
 logLik<-function(y, lambda){
   n<-length(y)
   #Getting the n of the function
