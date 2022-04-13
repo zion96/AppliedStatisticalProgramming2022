5ec539d8b6581ec6086f75bdb8056139fc29a1db
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-13 01:22:14 -0500

    estimatePois documentation

--- Exam/easyPois/R/estimatePois.R
+++ Exam/easyPois/R/estimatePois.R
@@ -1,3 +1,30 @@
+#'Estimate the Poisson model
+#'
+#'For a vector of strictly non-negative entries, this returns an object in the class of
+#'\code{PoisMLE} and \eqn{\hat \lambda}.
+#'
+#'@param y is a vector for which all entries are strictly non-negative reals
+#'@param SEtype Standard error calculation method
+#'@param B Is an integer that I have set to 20 as the default. This is not used when \code{"basic"} is selected.
+#'
+#'@return Object in \code{"PoisMLE"} with the following:
+#'#'\itemize{
+#'\item \code{y} is a vector for which all entries are strictly non-negative reals
+#'\item \code{MLE} Poisson MLE for an entry of y
+#'\item \code{LL} Log likelihood from data in y using Poisson MLE estimator
+#'\item \code{SE} Standard error of Poisson MLE
+#'\item \code{SEtype} Standard error calculation method
+#'}
+#'@author Zion Little
+#'
+#'@examples
+#'\donttest{the_y<-c(1:20)
+#'estimatePois(the_y, "basic", B=20)}
+#'
+#'@rdname estimatePois
+#'@aliases estimatePois
+#'
+#'@export
 setGeneric(
   name = "estimatePois",
   definition = function(y, SEtype, B=20){
@@ -5,7 +32,9 @@ setGeneric(
   }
 )
 
-
+#'@rdname estimatePois
+#'
+#'@export
 setMethod(
   "estimatePois",
   definition = function(y, SEtype, B=20){
