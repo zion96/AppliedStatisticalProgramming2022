59e2fc497535cfee53f289d573871cc2f40f8879
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-13 00:58:28 -0500

    standardError documentation

--- Exam/easyPois/R/StandardError.R
+++ Exam/easyPois/R/StandardError.R
@@ -1,3 +1,31 @@
+#'Calculate the standard error of \code{PoisMLE}
+#'
+#'For internal use. The function below finds the standard error of the MLE for object of the class \code{PoisMLE}.
+#'The user selects an argument for which method to use in the calculation. The two methods are \code{"basic"} and \code{\"bootstrap}.
+#'The following calculations are run, respectively:
+#'\deqn{\sqrt(\frac{MLE}{n})} ; standard deviation of \code{B} bootstraps of \code{y}.
+#'The output value is returned when object in class \code{PoisMLE} is initialized.
+#'
+#'@param y is a vector for which all entries are strictly non-negative reals
+#'@param SEtype is entries of either \code{"basic"} or \code{"bootstrap"}. See description.
+#'@param B Is an integer that I have set to 20 as the default. This is not used when \code{"basic"} is selected.
+#'
+#'@return the standard errors
+#'
+#'@example
+#'y<-c(1:10)
+#'basic<- standardError(y = y, SEtype= "basic", B=20)
+#'bootstrap<-standardError(y = y, SEtype= "bootstrap", B=20)
+#'
+#'@author Zion Little
+#'@seealso PoisMLE-class, logLik, MLE
+#'@rdname StandardError
+#'@aliases StandardError
+#'
+#'@keywords internal
+#'
+#'@export
+
 
 standardError<-function(y, SEtype, B=20){
   #Run the if statement when SEtype is basic
