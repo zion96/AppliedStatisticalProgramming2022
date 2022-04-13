9bda6950537c255c1643b17aed3e3e15f5c11ee0
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-12 21:25:11 -0500

    more comments
    wondering if I need to have another function to get the bootstrap sample.

--- Exam/easyPois/R/StandardError.R
+++ Exam/easyPois/R/StandardError.R
@@ -1,4 +1,4 @@
-#Now, I'll just create this function as if we can only do SEtype=basic
+
 standardError<-function(y, SEtype, B=20){
     n<-length(y)
     #Getting n again
@@ -6,5 +6,11 @@ standardError<-function(y, SEtype, B=20){
     the_se<-sqrt(mle(y)/n)
     #this is just the se assuming that I'm setting the method to pass the mle through here.
     #I'm not too sure about this yet.
+
+    the_samples<-matrix(unlist())
+
+    #Okay now I need to think about whether I want another function in here.
+
+
 }
 
