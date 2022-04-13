7b70f53d681d3330d3922c774791f31d36bc1016
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-13 00:17:14 -0500

    MLE documentation

--- Exam/easyPois/R/MLE.R
+++ Exam/easyPois/R/MLE.R
@@ -1,3 +1,30 @@
+#'MLE for \equn{\lambda} in object of class \code{PoisMLE}
+#'
+#'For internal use. The function below runs the MLE calculation for Poisson rate \eqn{\lambda}. The following
+#'calculation is performed:
+#'\deqn{\frac{\lambda^{y_{i} e(-\lambda)} }{y_{i}!}}, which defines \deqn{\P(Y_{i}=y)}.
+#'The estimator is used and returned when an object in the class \code{PoisMLE} is initialized.
+#'
+#'@param y is a vector for which all entries are strictly non-negative reals
+#'
+#'@return MLE
+#'
+#'@example
+#'y<-c(1:20)
+#'y_mle<-mle(y)
+#'
+#'
+#'@author Zion Little
+#'@seealso PoisMLE-class, logLik,StandardError
+#'@rdname MLE
+#'@aliases MLE
+#'
+#'@keywords internal
+#'
+#'@export
+
+
+
 mle<-function(y){
   n<-length(y)
   #Getting n, again.
