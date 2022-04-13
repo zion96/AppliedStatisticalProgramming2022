679f2f9972fcfc8a4a276a8bebf80117e08aecd8
zion96 <99201224+zion96@users.noreply.github.com>
2022-04-12 21:36:53 -0500

    lapply!!!
    nevermind, lapply exists.

--- Exam/easyPois/R/StandardError.R
+++ Exam/easyPois/R/StandardError.R
@@ -7,9 +7,7 @@ standardError<-function(y, SEtype, B=20){
     #this is just the se assuming that I'm setting the method to pass the mle through here.
     #I'm not too sure about this yet.
 
-    the_samples<-function(m){
-
-    }
+    the_samples<-matrix(unlist(lapply(c(1:B))))
 
     #Okay now I need to think about whether I want another function in here.
 
